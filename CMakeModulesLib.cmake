848e09a4b8690159241ba2b3346477c69b2eeaa8
Andreas Kunz <andreas.kunz@ost.ch>
2021-03-16 17:33:18 +0100

    Improve checkout lib version function

--- CMakeModulesLib.cmake
+++ CMakeModulesLib.cmake
@@ -11,13 +11,20 @@ include(Logger)
 
 # Checks out the specified version of the library
 function(checkout_cmake_modules_lib_version version)
-  execute_process(COMMAND ${GIT_EXECUTABLE} checkout ${version}
-                  WORKING_DIRECTORY ${CMAKE_MODULES_LIB_PATH}
-                  RESULT_VARIABLE CMAKE_MODULES_LIB_CHECKOUT_RESULT
-                  ERROR_VARIABLE CMAKE_MODULES_LIB_CHECKOUT_OUTPUT)
-  if(NOT CMAKE_MODULES_LIB_CHECKOUT_RESULT EQUAL "0")
-    message(FATAL_ERROR "failed command: git checkout ${version}\n ${CMAKE_MODULES_LIB_CHECKOUT_OUTPUT}")
+  if(NOT LIB_CMAKE_MODULES_ROOT_PATH)
+    log("Could not checkout ${version} since LIB_CMAKE_MODULES_ROOT_PATH not set.")
+    return()
   endif()
+
+  execute_process(COMMAND ${GIT_EXECUTABLE} checkout ${version}
+                  WORKING_DIRECTORY ${LIB_CMAKE_MODULES_ROOT_PATH}
+                  RESULT_VARIABLE LIB_CMAKE_MODULES_CHECKOUT_RESULT
+                  ERROR_VARIABLE LIB_CMAKE_MODULES_CHECKOUT_OUTPUT)
+
+  if(NOT LIB_CMAKE_MODULES_CHECKOUT_RESULT EQUAL "0")
+    message(FATAL_ERROR "failed command: git checkout ${version}\n ${LIB_CMAKE_MODULES_CHECKOUT_OUTPUT}")
+  endif()
+
   log("Using cmake-modules library version:${version}")
 endfunction()
 
